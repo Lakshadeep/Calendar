9bf9f498849d55cabbb844bfa9c586fa95746f58
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-09 23:26:26 +0530

    Specific day format selection update

--- Calendar/part1.rb
+++ Calendar/part1.rb
@@ -70,7 +70,13 @@ def create_calendar(month,year,day_format,last_day)
 	last_month_year = temp[1]
 	last_month_day = get_days_in_month(last_month,last_month_year)
 
-	last_month_count = (day_format - get_weekday(month,year,$day)).abs
+	last_month_count = get_weekday(month,year,$day) - day_format
+
+	if last_month_count < 0
+		last_month_count = (7 + last_month_count)
+	else
+		last_month_count = last_month_count
+	end
 
 	next_month_count = 1
 	
@@ -123,45 +129,54 @@ end
 
 
 $x = '1'
-$day_from = 0
+$day_form = 0
 $today = get_today_info
 
 while $x != 'q' do
-	
 	puts("\n")
-	puts("To go to next month press N\n")
-	puts("To go to previous month press P\n")
+	puts("To go to next month press n\n")
+	puts("To go to previous month press p\n")
+	puts("To change start-day press w \n")
 	puts("\n")
 	
-	
-
 	if $x == '1'
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],0,last_day),days7,0)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
 		$x = STDIN.getch
 	elsif $x.eql?"p"
-	
 		temp = get_previous_month($today[1],$today[2])
 		$today[1] = temp[0]
 		$today[2] = temp[1]
-
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],0,last_day),days7,0)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
 		$x = STDIN.getch
 	elsif $x.eql?"n"
 		temp = get_next_month($today[1],$today[2])
 		$today[1] = temp[0]
-		$today[2] = temp[1]
-		
+		$today[2] = temp[1]	
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],0,last_day),days7,0)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
 		$x = STDIN.getch
+	elsif $x.eql?"w"
+		puts("\n")
+		puts("Enter 0 for Sunday\n")
+		puts("Enter 1 for Monday\n")
+		puts("Enter 2 for Tuesday\n")
+		puts("Enter 3 for Wednesday\n")
+		puts("Enter 4 for Thursday\n")
+		puts("Enter 5 for Friday\n")
+		puts("Enter 6 for Saturday\n")
+		puts("\n")	
+		$day_form = gets.to_i
+		puts($day_form)
+
+		puts("\n")
+		puts("To go to next month press n\n")
+		puts("To go to previous month press p\n")
+		puts("To change start-day press w \n")
+		puts("\n")	
+		$x = STDIN.getch	
 	else
-
-		
 		$x = STDIN.getch
-
-
 	end
-
 end		
