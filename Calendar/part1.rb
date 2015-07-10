8f8b17945c43b435dc2c623a1d7404d85cc11ce8
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-10 12:46:44 +0530

    Holidays display update

--- Calendar/part1.rb
+++ Calendar/part1.rb
@@ -1,7 +1,51 @@
 require 'date'
 require 'io/console'
 
+$holidays_list = [[1,1,2015,"Happy New Year",""],
+				 [26,1,2015,"Republic Day",""],
+				 [19,4,2015,"My Birthday",""],	
+				 [19,7,2015,"Ramzan",""],
+				 [31,7,2015,"Jamat Ul Vida",""],
+				 [15,8,2015,"Independence Day",""],
+				 [28,8,2015,"Onam",""],
+				 [17,9,2015,"Ganesh Chaturthi",""],
+				 [22,10,2015,"Dussehra",""],
+				 [11,11,2015,"Diwali",""],
+				 [25,12,2015,"Christmas day",""],
+				 [1,1,2016,"Happy New Year",""],
+				 [26,1,2016,"Republic Day",""]]
+
 days7 = %w[ sun  mon  tue  wed  thu  fri  sat]
+
+def get_holiday_list(month ,year,holidays)
+	temp = []
+	ch = 65
+	for i in 0..holidays.length-1
+		if holidays[i][1] == month and holidays[i][2] == year
+			holidays[i][4] = ch
+			ch = ch + 1
+			temp.push(holidays[i])
+		end
+		
+	end
+	return temp 
+end
+
+def holiday_print(holiday_l,day)
+	space = 32
+	if holiday_l.length != 0
+		for i in 0..holiday_l.length-1
+			if holiday_l[i][0] == day
+				return holiday_l[i][4]
+			
+				
+			end
+		end
+
+	end
+	return space
+end
+
 
 def get_today_info
 	time = Time.new
@@ -49,16 +93,6 @@ def get_next_month(month,year)
 	end
 end
 
-$weekdays = {
-  0  => 'Mon',
-  1  => 'Tue',
-  2  => 'Wed',
-  3  => 'Thu',
-  4  => 'Fri',
-  5  => 'Sat',
-  6  => 'Sun'
-}
-
 
 def create_calendar(month,year,day_format,last_day)
 	$day = 1
@@ -79,16 +113,19 @@ def create_calendar(month,year,day_format,last_day)
 	end
 
 	next_month_count = 1
-	
+
+	#part 4 update
+	holidays = get_holiday_list(month,year,$holidays_list)
+
 
 	for i in 0..5
 		for j in 0..6
 			if $day <= last_day
 				if ((j+day_format)%7) == get_weekday(month,year,$day)
 					if $day < 10
-						$month_array[i][j] = "  "+$day.to_s+" "
+						$month_array[i][j] = "  "+$day.to_s+holiday_print(holidays,$day).chr
 					else
-						$month_array[i][j] = " "+$day.to_s+" "
+						$month_array[i][j] = " "+$day.to_s+holiday_print(holidays,$day).chr
 					end
 					$day = $day + 1
 				else
@@ -113,8 +150,9 @@ def headers_dispaly(weekday,start)
 	puts(" "+weekday[start%7]+ "  "+weekday[(start+1)%7]+ "  "+weekday[(start+2)%7]+ "  "+weekday[(start+3)%7]+ "  "+weekday[(start+4)%7]+ "  "+weekday[(start+5)%7]+ "  "+weekday[(start+6)%7])
 end
 
-def display_calendar(x,weekday,start_day)
-
+def display_calendar(x,weekday,start_day,month,year)
+	months = %w[Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec]
+	puts(months[month-1] +"  "+ year.to_s)
 	headers_dispaly(weekday,start_day)
 	
 	puts "\n"
@@ -123,6 +161,12 @@ def display_calendar(x,weekday,start_day)
 	
 		puts x[i].join(' ')
 	
+	end
+
+	holidays = get_holiday_list(month,year,$holidays_list)
+	for i in 0..holidays.length-1
+		puts(holidays[i][4].chr + " " + holidays[i][3])
+		puts("\n")
 	end
 end
 
@@ -133,6 +177,7 @@ $day_form = 0
 $today = get_today_info
 
 while $x != 'q' do
+	puts "\e[H\e[2J"
 	puts("\n")
 	puts("To go to next month press n\n")
 	puts("To go to previous month press p\n")
@@ -141,21 +186,21 @@ while $x != 'q' do
 	
 	if $x == '1'
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form,$today[1],$today[2])
 		$x = STDIN.getch
 	elsif $x.eql?"p"
 		temp = get_previous_month($today[1],$today[2])
 		$today[1] = temp[0]
 		$today[2] = temp[1]
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form,$today[1],$today[2])
 		$x = STDIN.getch
 	elsif $x.eql?"n"
 		temp = get_next_month($today[1],$today[2])
 		$today[1] = temp[0]
 		$today[2] = temp[1]	
 		last_day = get_days_in_month($today[1],$today[2])
-		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form)
+		display_calendar(create_calendar($today[1],$today[2],$day_form,last_day),days7,$day_form,$today[1],$today[2])
 		$x = STDIN.getch
 	elsif $x.eql?"w"
 		puts("\n")
