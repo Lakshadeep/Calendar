37ab8854078c2816d817c1f9223927f6e19183f7
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-13 13:03:52 +0530

    Updated afyer 2nd review

--- Calendar/part1_v2.rb
+++ Calendar/part1_v2.rb
@@ -7,38 +7,47 @@ def get_days_in_month(month,year)
   return no_of_days
 end
 
-def create_calendar(date,start_day)
+def create_calendar(date,first_dow)
 	day = 1
 	c_month = date.mon
 	c_year = date.year
 	
-	# REVIEW -- You can get these names from the Date class itself
-	months = %w[Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec]
-	days7 = %w[ sun  mon  tue  wed  thu  fri  sat]
+	# REVIEW -- You can get these names from the Date class itself    --fixed
+	m_temp = Date::ABBR_MONTHNAMES
+	months = m_temp[1,13]
+	days7 = Date::ABBR_DAYNAMES
 
 	# REVIEW -- use the #{} string interpolation instead of using + to
-	# concatenate. eg "My name is #{name}". Why haven't you used printf to align
+	# concatenate. eg "My name is #{name}". Why haven't you used printf to align   --fixed
 	# output?
-	puts("             "+months[c_month-1] +"  "+ c_year.to_s)
-	headers_display(days7,start_day)
+	printf("%25s","#{ months[c_month-1]}, #{c_year.to_s}\n")
+	headers_display(days7,first_dow)
 	last_day = get_days_in_month(date.mon,date.year)
+	start_day = 0 
 
+	# REVIEW -- why is this condition being evaluated everytime inside the
+				# loop?                                                       --fixed
+	for i in 0..6
+		if ((i+first_dow)%7) == (Date.new(date.year,date.mon,day)).wday
+			start_day = i
+			break
+		end
+	end
 
 	for i in 0..5
 		for j in 0..6
 			# REVIEW -- try to prevent nested if conditions. Try converting them to
-			# if-elsif blocks
-			if day <= last_day
-				# REVIEW -- why is this condition being evaluated everytime inside the
-				# loop?
-				if ((j+start_day)%7) == (Date.new(date.year,date.mon,day)).wday
-					printf("%5s",day.to_s)
-					day = day + 1
-				else
-					printf("%5s","")		
-				end
+			# if-elsif blocks                                                       --fixed
+
+			if day <= last_day and j >= start_day  and  i ==0
+				printf("%5s",day.to_s)
+				day = day + 1
+			elsif day <= last_day and i!=0
+				printf("%5s",day.to_s)
+				day = day + 1
 			else
-				printf("%5s","")				
+				printf("%5s","") 
+
 			end
 		end
 		puts()	
@@ -47,7 +56,7 @@ def create_calendar(date,start_day)
 end
 
 def headers_display(weekday,start)
-	puts("  "+weekday[start%7]+ "  "+weekday[(start+1)%7]+ "  "+weekday[(start+2)%7]+ "  "+weekday[(start+3)%7]+ "  "+weekday[(start+4)%7]+ "  "+weekday[(start+5)%7]+ "  "+weekday[(start+6)%7])
+	puts("  #{weekday[start%7]}  #{weekday[(start+1)%7]}  #{weekday[(start+2)%7]}  #{weekday[(start+3)%7]}  #{weekday[(start+4)%7]}  #{weekday[(start+5)%7]}  #{weekday[(start+6)%7]}")
 end
 
 #main program starts from here
